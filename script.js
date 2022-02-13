8cdf0841bdf0d01c4f1b5d428bfcae0c4f7da690
Diego H <d13g01hl@gmail.com>
2022-02-13 13:31:52 -0500

    Added home method

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ let templates = {};
 let app_div = document.getElementById('app');
 
 let home = () => {
-
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+    link.href = '#/about';
+    link.innerText = 'About';
+    div.innerHTML = '<h1>Home</h1>';
+    div.appendChild(link);
+    app_div.appendChild(div);
 };
 
 let about = () => {
